e69e48cbaccc237eb7b337b161c1b1d0a94ce108
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2022-04-05 13:19:31 +0300

    separating html
    separating html

--- ng-workshop/src/app/app.module.ts
+++ ng-workshop/src/app/app.module.ts
@@ -9,18 +9,22 @@ import { ThemeListComponent } from './theme-list/theme-list.component';
 import { AsideComponent } from './aside/aside.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    ThemeListComponent,
-    AsideComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		ThemeListComponent,
+		AsideComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent
+	]
 })
 export class AppModule { }
